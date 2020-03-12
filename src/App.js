8fbf1fa5d684411923177d17b696fb2ff8bd5d0a
Rodrigo Arcanjo <rodrigo.arcanjo@afferolab.com.br>
2020-03-12 20:52:04 -0300

    Estilos globais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from './styles/global';
+import Routes from './routes';
 
 function App() {
-  return <h1>Hello Word</h1>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes />
+      <GlobalStyles />
+    </BrowserRouter>
+  );
 }
 
 export default App;
